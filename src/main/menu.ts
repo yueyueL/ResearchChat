e9dfddd09c53332810690cdd2fd1aacaabcee7d0
yueyueL <yue.liu1@monash.edu>
2024-09-06 15:06:46 +1000

    update icon and menu

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -54,17 +54,17 @@ export default class MenuBuilder {
 
     buildDarwinTemplate(): MenuItemConstructorOptions[] {
         const subMenuAbout: DarwinMenuItemConstructorOptions = {
-            label: 'Chatbox',
+            label: 'ChatResearch',
             submenu: [
                 {
-                    label: 'About Chatbox',
+                    label: 'About ChatResearch',
                     selector: 'orderFrontStandardAboutPanel:',
                 },
                 { type: 'separator' },
                 { label: 'Services', submenu: [] },
                 { type: 'separator' },
                 {
-                    label: 'Hide Chatbox',
+                    label: 'Hide ChatResearch',
                     accelerator: 'Command+H',
                     selector: 'hide:',
                 },
